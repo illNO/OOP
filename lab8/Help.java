d20aadb55a3b1578d062a5e14e6d36f896758457
illNO <36901640+illNO@users.noreply.github.com>
2018-05-13 23:18:57 +0300

    Create Help.java

---
+++ lab8/Help.java
@@ -0,0 +1,6 @@
+package com.company;
+public class Help extends Main{
+    public void execute(){
+        System.out.println("This program was created by Igor Shevchuk. The aim was to swap words with constant lenght by new line");
+    }
+}
